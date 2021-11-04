77ff7e2f0d5a6127953e15dfa5909448881499b8
TanyaRan <tatyanaran@gmail.com>
2021-11-05 00:20:33 +0200

    fix cart reducer

--- src/context/cart-reducer.js
+++ src/context/cart-reducer.js
@@ -1,12 +1,14 @@
 const cartReducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_ITEM':
-      // check if item is in cart
-      if (!state.cartItems.find(item => item.id === action.payload.id)) {
-      }
-    default:
-      return state;
-  }
+	switch (action.type) {
+		// case 'ADD_ITEM':
+		// 	// check if item is in cart
+		// 	if (!state.cartItems.find(item => item.id === action.payload.id)) {
+		//
+		// 		});
+		// 	}
+		default:
+			return state;
+	}
 };
 
 export default cartReducer;
